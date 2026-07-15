09ed7d5d7e3a19763920d3b5a306e4908ba17993
eneche-web <enechegizo@gmail.com>
2026-07-15 03:09:50 -0700

    improved

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,9 @@
-const menuButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
+const menuButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
 
-menuButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-    menuButton.classList.toggle("open");
+menuButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    menuButton.classList.toggle('open');
     }
 
 );
